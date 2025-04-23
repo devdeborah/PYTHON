3739ad7b9eee50f574b5611d55b5e66e995a7d47
Deborah Reis <146981071+devdeborah@users.noreply.github.com>
2025-04-23 00:03:31 -0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# Imprime as informações das variáveis na tela.
+nome = 'Deborah'
+sobrenome = 'Reis'
+idade = 20
+anoNascimento = 2005
+maioridade = True
+altura = 1.64
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', anoNascimento)
+print('É maior de idade?', maioridade)
+print('Altura em metros:', altura)
